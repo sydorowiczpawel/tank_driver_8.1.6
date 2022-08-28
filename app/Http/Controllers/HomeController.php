98c6edf33a771373f7a8ba793f2568ed6772d449
Paweł Sydorowicz <pawelsydorowicz@gmail.com>
2022-08-28 16:02:15 +0200

    dla zalogowanego użytkownika przycisk HOME zawiera informacje kto jest zalogowany

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = DB::table('users')
+        ->get();
+
+        return view('welcome')
+        ->with('user', $user);
     }
 }
