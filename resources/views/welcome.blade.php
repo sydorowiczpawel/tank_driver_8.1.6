0b96977fe2cf7bc8d18944aed61f19532466e254
Paweł Sydorowicz <pawelsydorowicz@gmail.com>
2022-09-06 22:16:07 +0200

    usunięcie podlinkowania numeru przepustki

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 <div class="container">
 @if(Auth::user())
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">{{  Auth::user() -> pass_number }}</a>
+  <a class="navbar-brand">{{  Auth::user() -> pass_number }}</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
